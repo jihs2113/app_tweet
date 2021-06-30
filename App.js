69edbb849fde29dab0a028e5aedc7bc9d880f224
Terry_Kim <48509331+jihs2113@users.noreply.github.com>
2021-07-01 02:50:22 +0900

    setting success

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Auth from "./components/Auth";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  render(){
+
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your own app!</Text>
+        <Auth/>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
